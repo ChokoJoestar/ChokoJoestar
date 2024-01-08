2e2f68c5b2d9d7ed30f31185fc57b5c6439cbf00
ChokoJoestar <chokojoestar@gmail.com>
2024-01-08 20:59:13 +0100

    Update Optimization.ps1

--- Optimization.ps1
+++ Optimization.ps1
@@ -1,18 +1,3 @@
-param([switch]$Elevated)
-function Test-Admin {
-   $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-   $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-if ((Test-Admin) -eq $false) {
-   if ($elevated) {
-      Write-Output "Elevating did not work :("
-   }
-   else {
-      Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-   }
-   exit
-}
 function makeLogFile {
    if (!(Test-Path C:\ChokoJoestar\Optimization.log -PathType Leaf)) {
       New-Item "C:\ChokoJoestar" -ItemType Directory
